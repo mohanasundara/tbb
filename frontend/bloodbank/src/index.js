b831285aeb64c88a7691bce2ca1ded927ad748b4
babu <mohansundaramm400@gmail.com>
2023-08-20 15:48:00 +0530

    pwa app fixed

--- frontend/bloodbank/src/index.js
+++ frontend/bloodbank/src/index.js
@@ -7,7 +7,7 @@ import {Provider} from 'react-redux';
 import { store } from './features/Store';
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
- 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 const persistor = persistStore(store);
@@ -29,6 +29,12 @@ root.render(
        
   </React.StrictMode>
 );
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
