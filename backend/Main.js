4e0813a5cc5887eb2673ba41dfe9d0377ad1867c
babu <mohansundaramm400@gmail.com>
2023-10-21 18:01:11 +0530

    mongourl

--- backend/Main.js
+++ backend/Main.js
@@ -46,5 +46,6 @@ app.listen(8000,()=>{
 
 
 //database
-mongoose.connect('mongodb+srv://babu:babu@bloodbank.vadqhsx.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://babu:babu@bloodbank.om1hyyw.mongodb.net/?retryWrites=true&w=majority')
+
   .then(() => console.log('Connected!'));
